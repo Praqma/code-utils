5a754ff1f839ab80fe1f48094de436fd1a1667cc
Claus Schneider  (Praqma) <cls@praqma.net>
2019-12-04 17:41:44 +0100

    git repo analyzer: fix space in filenames and fail on errors

--- git-repo-analyzer/git-object-sizes-in-repo-analyzer.sh
+++ git-repo-analyzer/git-object-sizes-in-repo-analyzer.sh
@@ -1,7 +1,9 @@
-#!/bin/bash -e -x
+#!/bin/bash
 
 set -x
 set -e
+
+[[ ${repack} == "" ]] && repack="true"
 
 set +x
 export PATH=/cygdrive/c/Program\ Files\ \(x86\)/Git/bin:${PATH}
@@ -53,37 +55,39 @@ fi
 [[ -d .git/modules ]] && du -sh .git/modules
 du -sh .git
 
-# reference: https://stackoverflow.com/questions/28720151/git-gc-aggressive-vs-git-repack
-git reflog expire --all --expire=now
-git repack -a -d --depth=250 --window=250 # accept to use old deltas - add "-f" option to not reuse old deltas for large repos it fails often
-git gc --prune
+if [[ ${repack} == "true" ]]; then
+  # reference: https://stackoverflow.com/questions/28720151/git-gc-aggressive-vs-git-repack
+  git reflog expire --all --expire=now
+  git repack -a -d --depth=250 --window=250 # accept to use old deltas - add "-f" option to not reuse old deltas for large repos it fails often
+  git gc --prune
+fi
 
 [[ -d .git/objects ]] && du -sh .git/objects
 du -sh .git
 
-git rev-list --objects --all | sort -k 2 | sed -e 's/ /@/g' -e 's/@/ /' -e 's/(//g' -e 's/)//g'  > ${WORKSPACE}/allfileshas.txt
-cat ${WORKSPACE}/allfileshas.txt| cut -f 2 -d\  | uniq > ${WORKSPACE}/allfileshas_uniq.txt
+git rev-list --objects --all  > ${WORKSPACE}/allfileshas.txt
+cat ${WORKSPACE}/allfileshas.txt| cut -d ' ' -f 2-  | uniq > ${WORKSPACE}/allfileshas_uniq.txt
 
 export pack_file=$(find ${pack_dir} -name '*.idx')
 git verify-pack -v ${pack_file} | egrep "^\w+ blob\W+[0-9]+ [0-9]+ [0-9]+$" | awk -F" " '{print $1,$2,$3,$4,$5}' > ${WORKSPACE}/bigobjects.txt
 git verify-pack -v ${pack_file} | egrep "^\w+ blob\W+[0-9]+ [0-9]+ [0-9]+ [0-9]+" | awk -F" " '{print $1,$2,$3,$4,$5}' > ${WORKSPACE}/bigobjects_revisions.txt
 
-#cat ${WORKSPACE}/bigobjects.txt | sort -k 3 -n -r > ${WORKSPACE}/bigobjects_sorted.txt
+join <(sort ${WORKSPACE}/bigobjects.txt) <(sort ${WORKSPACE}/allfileshas.txt) | sort -k 3 -n -r | cut -f 1,3,6-  -d ' ' > ${WORKSPACE}/bigtosmall_join.txt
 
-
-join <(sort ${WORKSPACE}/bigobjects.txt) <(sort ${WORKSPACE}/allfileshas.txt) | sort -k 3 -n -r | cut -f 1,3,6- -d \ > ${WORKSPACE}/bigtosmall_join.txt
-
-join <(sort ${WORKSPACE}/bigobjects_revisions.txt) <(sort ${WORKSPACE}/allfileshas.txt) | sort -k 3 -n -r | cut -f 1,3,6- -d \ > ${WORKSPACE}/bigtosmall_revisions_join.txt
+join <(sort ${WORKSPACE}/bigobjects_revisions.txt) <(sort ${WORKSPACE}/allfileshas.txt) | sort -k 3 -n -r | cut -f 1,3,6- -d ' '  > ${WORKSPACE}/bigtosmall_revisions_join.txt
 
 touch ${WORKSPACE}/bigtosmall_join_uniq.txt
 set +x
-for file in `cat ${WORKSPACE}/bigtosmall_join.txt | awk -F " " '{print $3}'` ; do
+
+IFS=$'\r\n'
+for file in `cat ${WORKSPACE}/bigtosmall_join.txt | cut -d ' ' -f 3- ` ; do
   grep -e "^${file}$" ${WORKSPACE}/bigtosmall_join_uniq.txt || echo $file >> ${WORKSPACE}/bigtosmall_join_uniq.txt
 done
 set -x 
 
 touch ${WORKSPACE}/bigtosmall_errors.txt
 set +x
+IFS=$'\r\n'
 for file in `cat ${WORKSPACE}/bigtosmall_join_uniq.txt` ; do
 	grep -e " ${file}$" ${WORKSPACE}/bigtosmall_join.txt >> ${WORKSPACE}/bigtosmall_sorted_size_files.txt || echo "ERROR: $file: something went wrong" >> ${WORKSPACE}/bigtosmall_errors.txt
 done
@@ -91,14 +95,25 @@ set -x
 
 touch ${WORKSPACE}/bigtosmall_revisions_join_uniq.txt
 set +x
-for file in `cat ${WORKSPACE}/bigtosmall_revisions_join.txt | awk -F " " '{print $3}'` ; do
+IFS=$'\r\n'
+for file in `cat ${WORKSPACE}/bigtosmall_revisions_join.txt | cut -d ' ' -f 3- ` ; do
   grep -e "^${file}$" ${WORKSPACE}/bigtosmall_revisions_join_uniq.txt || echo $file >> ${WORKSPACE}/bigtosmall_revisions_join_uniq.txt
 done
 set -x
 
 touch ${WORKSPACE}/bigtosmall_errors_revision.txt
 set +x
+IFS=$'\r\n'
 for file in `cat ${WORKSPACE}/bigtosmall_revisions_join_uniq.txt ` ; do
   grep -e " ${file}$" ${WORKSPACE}/bigtosmall_revisions_join.txt >> ${WORKSPACE}/bigtosmall_sorted_size_files_revisions.txt || echo "ERROR: $file: something went wrong" >> ${WORKSPACE}/bigtosmall_errors_revision.txt
 done
+set -x
+
+
+set +x
+if [[ ! `cat ${WORKSPACE}/bigtosmall_errors_revision.txt ${WORKSPACE}/bigtosmall_errors.txt` == ""  ]]; then
+  echo "There are errors during analyzing the files:  "
+  cat ${WORKSPACE}/bigtosmall_errors_revision.txt ${WORKSPACE}/bigtosmall_errors.txt | uniq
+  exit 1
+fi
 set -x
