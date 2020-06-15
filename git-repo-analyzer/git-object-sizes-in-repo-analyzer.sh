3d98b4bef3a028e9b75caeeb18add44d804ea95c
Claus Schneider (Eficode-Praqma) <claus.schneider@eficode.com>
2020-06-15 04:35:21 -0700

    git repo analyzer: reorder process root and revisions and less verbosive

--- git-repo-analyzer/git-object-sizes-in-repo-analyzer.sh
+++ git-repo-analyzer/git-object-sizes-in-repo-analyzer.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-set -x
 set -e
+set -u 
 
-[[ ${repack} == "" ]] && repack="true"
+[[ ${debug:-} ]] && set -x
+
+[[ ${repack:-} == "" ]] && repack=true
 
 set +x
 export PATH=/cygdrive/c/Program\ Files\ \(x86\)/Git/bin:${PATH}
@@ -23,19 +25,16 @@ which find
 which sort
 
 echo $PATH
-if [ "${WORKSPACE}X" == "X" ]; then
+if [[ "${WORKSPACE:-}" == "" ]]; then
   export WORKSPACE=`pwd`
 fi
-if [ "${1}x" != "x" ]; then
+if [[ "${1:-}" != "" ]]; then
   test -e ${1} && cd ${1}
 fi
 echo
 echo "Analyzing git in: `pwd` "
 echo "Saving outfiles in: ${WORKSPACE}"
 echo
-
-set -x
-set -u
 
 rm -f ${WORKSPACE}/bigtosmall_*.txt
 rm -f ${WORKSPACE}/bigobjects*.txt
@@ -69,50 +68,68 @@ git rev-list --objects --all  > ${WORKSPACE}/allfileshas.txt
 cat ${WORKSPACE}/allfileshas.txt| cut -d ' ' -f 2-  | uniq > ${WORKSPACE}/allfileshas_uniq.txt
 
 export pack_file=$(find ${pack_dir} -name '*.idx')
+
+echo "START: Investigate blobs that are directly stored in idx file: ${pack_file}"
 git verify-pack -v ${pack_file} | egrep "^\w+ blob\W+[0-9]+ [0-9]+ [0-9]+$" | awk -F" " '{print $1,$2,$3,$4,$5}' > ${WORKSPACE}/bigobjects.txt
-git verify-pack -v ${pack_file} | egrep "^\w+ blob\W+[0-9]+ [0-9]+ [0-9]+ [0-9]+" | awk -F" " '{print $1,$2,$3,$4,$5}' > ${WORKSPACE}/bigobjects_revisions.txt
 
 join <(sort ${WORKSPACE}/bigobjects.txt) <(sort ${WORKSPACE}/allfileshas.txt) | sort -k 3 -n -r | cut -f 1,3,6-  -d ' ' > ${WORKSPACE}/bigtosmall_join.txt
 
-join <(sort ${WORKSPACE}/bigobjects_revisions.txt) <(sort ${WORKSPACE}/allfileshas.txt) | sort -k 3 -n -r | cut -f 1,3,6- -d ' '  > ${WORKSPACE}/bigtosmall_revisions_join.txt
-
 touch ${WORKSPACE}/bigtosmall_join_uniq.txt
+#set +x
+#IFS=$'\r\n'
+#for file in `cat ${WORKSPACE}/bigtosmall_join.txt | cut -d ' ' -f 3- ` ; do
+#  grep -e "^${file}$" ${WORKSPACE}/bigtosmall_join_uniq.txt > /dev/null || echo $file >> ${WORKSPACE}/bigtosmall_join_uniq.txt
+#done
+#set -x 
+cat ${WORKSPACE}/bigtosmall_join.txt | cut -d ' ' -f 3- | /usr/bin/uniq >> ${WORKSPACE}/bigtosmall_join_uniq.txt
+
 set +x
-
-IFS=$'\r\n'
-for file in `cat ${WORKSPACE}/bigtosmall_join.txt | cut -d ' ' -f 3- ` ; do
-  grep -e "^${file}$" ${WORKSPACE}/bigtosmall_join_uniq.txt || echo $file >> ${WORKSPACE}/bigtosmall_join_uniq.txt
-done
-set -x 
-
+echo "Generate file sorted list:"
 touch ${WORKSPACE}/bigtosmall_errors.txt
-set +x
 IFS=$'\r\n'
 for file in `cat ${WORKSPACE}/bigtosmall_join_uniq.txt` ; do
+	printf "."
 	grep -e " ${file}$" ${WORKSPACE}/bigtosmall_join.txt >> ${WORKSPACE}/bigtosmall_sorted_size_files.txt || echo "ERROR: $file: something went wrong" >> ${WORKSPACE}/bigtosmall_errors.txt
 done
+printf "\n"
+echo "DONE: Investigate blobs that are directly stored in idx file: ${pack_file}"
 set -x
 
+echo "START: Investigate blobs that are packed in revisions in idx file: ${pack_file}"
+git verify-pack -v ${pack_file} | egrep "^\w+ blob\W+[0-9]+ [0-9]+ [0-9]+ [0-9]+" | awk -F" " '{print $1,$2,$3,$4,$5}' > ${WORKSPACE}/bigobjects_revisions.txt
+join <(sort ${WORKSPACE}/bigobjects_revisions.txt) <(sort ${WORKSPACE}/allfileshas.txt) | sort -k 3 -n -r | cut -f 1,3,6- -d ' '  > ${WORKSPACE}/bigtosmall_revisions_join.txt
 touch ${WORKSPACE}/bigtosmall_revisions_join_uniq.txt
-set +x
-IFS=$'\r\n'
-for file in `cat ${WORKSPACE}/bigtosmall_revisions_join.txt | cut -d ' ' -f 3- ` ; do
-  grep -e "^${file}$" ${WORKSPACE}/bigtosmall_revisions_join_uniq.txt || echo $file >> ${WORKSPACE}/bigtosmall_revisions_join_uniq.txt
-done
-set -x
 
-touch ${WORKSPACE}/bigtosmall_errors_revision.txt
+#set +x
+#IFS=$'\r\n'
+#for file in `cat ${WORKSPACE}/bigtosmall_revisions_join.txt | cut -d ' ' -f 3- ` ; do
+#  grep -e "^${file}$" ${WORKSPACE}/bigtosmall_revisions_join_uniq.txt > /dev/null || echo $file >> ${WORKSPACE}/bigtosmall_revisions_join_uniq.txt
+#done
+#set -x
+cat ${WORKSPACE}/bigtosmall_revisions_join.txt | cut -d ' ' -f 3- | /usr/bin/uniq >> ${WORKSPACE}/bigtosmall_revisions_join_uniq.txt
+
 set +x
+echo "Generate file sorted list:"
+touch ${WORKSPACE}/bigtosmall_errors_revision.txt
 IFS=$'\r\n'
 for file in `cat ${WORKSPACE}/bigtosmall_revisions_join_uniq.txt ` ; do
-  grep -e " ${file}$" ${WORKSPACE}/bigtosmall_revisions_join.txt >> ${WORKSPACE}/bigtosmall_sorted_size_files_revisions.txt || echo "ERROR: $file: something went wrong" >> ${WORKSPACE}/bigtosmall_errors_revision.txt
+	printf "."
+    grep -e " ${file}$" ${WORKSPACE}/bigtosmall_revisions_join.txt >> ${WORKSPACE}/bigtosmall_sorted_size_files_revisions.txt || echo "ERROR: $file: something went wrong" >> ${WORKSPACE}/bigtosmall_errors_revision.txt
 done
+printf "\n"
+echo "DONE: Investigate blobs that are packed in revisions in idx file: ${pack_file}"
 set -x
 
 
+echo "START: Investigate if issues occured"
 set +x
-if [[ ! `cat ${WORKSPACE}/bigtosmall_errors_revision.txt ${WORKSPACE}/bigtosmall_errors.txt` == ""  ]]; then
-  echo "There are errors during analyzing the files:  "
-  cat ${WORKSPACE}/bigtosmall_errors_revision.txt ${WORKSPACE}/bigtosmall_errors.txt | uniq
+if [[ ! `cat ${WORKSPACE}/bigtosmall_errors.txt` == ""  ]]; then
+  echo "There are errors during analyzing the files: ${WORKSPACE}/bigtosmall_errors.txt"
+  ${WORKSPACE}/bigtosmall_errors.txt | uniq
+fi
+if [[ ! `cat ${WORKSPACE}/bigtosmall_errors_revision.txt` == ""  ]]; then
+  echo "There are errors during analyzing the files: ${WORKSPACE}/bigtosmall_errors_revision.txt"
+  cat ${WORKSPACE}/bigtosmall_errors_revision.txt | uniq
 fi
 set -x
+echo "DONE: Investigate if issues occured"
