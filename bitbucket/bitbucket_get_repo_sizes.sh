d2902720e428f66e42b0afe5de2981de7e9ee214
Claus Schneider(Eficode) <claus.schneider@eficode.com>
2020-11-25 12:33:30 +0100

    Bitbucket: get_sizes update with internal id ( to find repo on disk ) and url for easy nagivation

--- bitbucket/bitbucket_get_repo_sizes.sh
+++ bitbucket/bitbucket_get_repo_sizes.sh
@@ -14,10 +14,12 @@ netrc_file=${2}
 [[ -f "${netrc_file}" ]] || { echo "Netrc file: ${netrc_file} does not exist"  && exit 1; }
 limit=100000
 
-printf "%-60s : %-20s : %-10s : %-10s : %-10s\n"  "project/repo-path" "bytes" "mbytes" "gbytes" "LFS status"
+printf "%-60s : %-20s : %-10s : %-10s : %-5s : %-5s %s\n"  "project/repo-path" "bytes" "mbytes" "gbytes" "LFS" "repo-id" "repo-URL"
 IFS=$'\r\n'
 for bitbucket_project in $(curl --fail --silent --insecure --netrc-file ${netrc_file} -X GET -H Content-Type:application/json -o - --url ${url}/rest/api/1.0/projects?limit=${limit} | jq -r .values[].key ); do
   for slug in $(curl --fail --silent --insecure --netrc-file ${netrc_file} -X GET -H Content-Type:application/json -o - --url ${url}/rest/api/1.0/projects/${bitbucket_project}/repos?limit=${limit} | jq -r .values[].slug ); do
+    repo_id=$(curl --fail --silent --insecure --netrc-file ${netrc_file} -X GET -H Content-Type:application/json -o - --url ${url}/rest/api/1.0/projects/${bitbucket_project}/repos/${slug} | jq .id )
+    repo_url=${url}/projects/${bitbucket_project}/repos/${slug}
     size_bytes=$(curl --fail --silent --insecure --netrc-file ${netrc_file} -X GET -H Content-Type:application/json -o - --url ${url}/projects/${bitbucket_project}/repos/${slug}/sizes | jq -r .repository)
     size_mb=$(awk '{printf "%d", $1/$2/$2}' <<< "$size_bytes 1024" )
     size_gb=$(awk '{printf "%d", $1/$2/$2/$2}' <<< "$size_bytes 1024" )
@@ -29,6 +31,6 @@ for bitbucket_project in $(curl --fail --silent --insecure --netrc-file ${netrc_
       lfs_status="+"
     fi
     unset _lfs_exit_code
-    printf "%-60s : %-20s : %-10s : %-10s : %-10s\n"  "${bitbucket_project}/repos/$slug" "${size_bytes}" "${size_mb}" "${size_gb}" "${lfs_status}"
+    printf "%-60s : %-20s : %-10s : %-10s : %-5s : %-5s : %s\n"  "${bitbucket_project}/repos/$slug" "${size_bytes}" "${size_mb}" "${size_gb}" "${lfs_status}" "${repo_id}" "${repo_url}"
   done
 done
