82d3be7563fed08ed7d9595dfae4151714ca2f1b
Claus Schneider(EfiCode) <claus.schneider@eficode.com>
2021-01-20 18:41:59 +0100

    Add options for skip size, handle bare and normal repo and partly new filenames

--- git-repo-analyzer/git-object-sizes-in-repo-analyzer.sh
+++ git-repo-analyzer/git-object-sizes-in-repo-analyzer.sh
@@ -71,13 +71,18 @@ fi
 echo
 
 if [[ $(git rev-parse --is-bare-repository) == true ]]; then
-  echo "repo_type=bare  ( bare / normal )"  echo
+  echo "repo_type=bare  ( bare / normal )"
   pack_dir="./objects"
   git_dir="."
+  branch_remote_option=""
+  default_branch=$(git branch  | grep '^* ' | cut -f 2 -d ' ')
+
 else
   echo "repo_type=normal ( bare / normal )"
   git_dir=".git"
   pack_dir=".git/objects"
+  branch_remote_option="-r"
+  default_branch=$(git branch -r |  grep origin/HEAD | cut -f 5 -d ' ')
 fi
 echo
 export pack_dir
@@ -86,11 +91,27 @@ echo "Analyzing git in: $(pwd) "
 echo "Saving outfiles in: ${WORKSPACE}"
 echo
 
+file_verify_pack="${WORKSPACE}/verify_pack.tmp" && rm -f "${file_verify_pack}"
+
 rm -f ${WORKSPACE}/bigtosmall_*.txt
 rm -f ${WORKSPACE}/bigobjects*.txt
 rm -f ${WORKSPACE}/allfileshas*.txt
-rm -f ${WORKSPACE}/branches_embedded.txt
-rm -f ${WORKSPACE}/branches_leaves.txt
+
+file_tmp_allfileshas="${WORKSPACE}/allfileshas.tmp" && rm -f "${file_tmp_allfileshas}"
+
+file_tmp_bigobjects="${WORKSPACE}/bigobjects.tmp" && rm -f "${file_tmp_bigobjects}"
+file_tmp_bigobjects_revisions="${WORKSPACE}/bigobjects_revisions.tmp" && rm -f "${file_tmp_bigobjects_revisions}"
+
+file_tmp_bigtosmall_join="${WORKSPACE}/bigobjects_join.tmp" && rm -f "${file_tmp_bigtosmall_join}"
+file_tmp_bigtosmall_join_revisions="${WORKSPACE}/bigobjects_join_revisions.tmp" && rm -f "${file_tmp_bigtosmall_join_revisions}"
+
+file_output_sorted_size_files="${WORKSPACE}/bigtosmall_sorted_size_files.txt" && rm -f "${file_output_sorted_size_files}"
+file_output_sorted_size_files_revisions="${WORKSPACE}/bigtosmall_sorted_size_files_revisions.txt" && rm -f "${file_output_sorted_size_files_revisions}"
+file_output_branch_embedded="${WORKSPACE}/branches_embedded.txt" && rm -f "${file_output_branch_embedded}"
+file_output_branch_leaves="${WORKSPACE}/branches_leaves.txt" && rm -f "${file_output_branch_leaves}"
+file_output_branch_embedded_tagged="${WORKSPACE}/branches_embedded_tagged.txt" && rm -f "${file_output_branch_embedded_tagged}
+}"
+file_output_branch_leaves_tagged="${WORKSPACE}/branches_leaves_tagged.txt" && rm -f "${file_output_branch_leaves_tagged}"
 
 
 printf "Clean old temp packs(if present): \n"
@@ -109,31 +130,48 @@ if [[ ${repack} == true ]]; then
   git reflog expire --all --expire=now
   git repack -a -d --depth=250 --window=250 # accept to use old deltas - add "-f" option to not reuse old deltas for large repos it fails often
   git gc --prune
-  echo "git repo and object sizes after repack:"
-  [[ -d "${git_dir}/${pack_dir}" ]] && du -sh "${git_dir}/${pack_dir}"
-  du -sh "${git_dir}"
+  if [[ ${skip_sizes:-} == "" ]]; then
+    echo "git repo and object sizes after repack:"
+    [[ -d "${git_dir}/${pack_dir}" ]] && du -sh "${git_dir}/${pack_dir}"
+    du -sh "${git_dir}"
+  else
+    echo "git repo and object sizes after repack: skipped"
+  fi
   pack_file=$(find ${pack_dir} -name '*.idx')
   [[ ${pack_file} ==  "" ]] && echo "No pack file available - exit 1" && exit 1
 else
   printf "repack == false - skip\n\n"
 
-  echo "git repo and object sizes:"
-  du -sh "${git_dir}"
-  [[ -d "${git_dir}/${pack_dir}" ]] && du -sh "${git_dir}/${pack_dir}"
+  if [[ ${skip_sizes:-} == "" ]]; then
+    echo "git repo and object sizes:"
+    du -sh "${git_dir}"
+    [[ -d "${git_dir}/${pack_dir}" ]] && du -sh "${git_dir}/${pack_dir}"
+  else
+    echo "git repo and object sizes: skipped"
+  fi
   echo
 fi
+if [[ ${skip_sizes:-} == "" ]]; then
+  echo "git lfs and modules sizes:"
+  [[ -d "${git_dir}/lfs" ]]     && du -sh "${git_dir}/lfs"     || echo "${git_dir}/lfs is not present"
+  [[ -d "${git_dir}/modules" ]] && du -sh "${git_dir}/modules" || echo "${git_dir}/modules is not present"
+else
+  echo "git lfs and modules sizes: skipped"
+fi
+
+
+
 export pack_file
 echo "Run verify-pack to list all objects in idx"
-git verify-pack -v "${pack_file}" > "${WORKSPACE}/verify_pack.txt"
+git verify-pack -v "${pack_file}" > "${file_verify_pack}"
 echo "Done"
 
-[[ -d "${git_dir}/lfs" ]]     && du -sh "${git_dir}/lfs"     || echo "${git_dir}/lfs is not present"
-[[ -d "${git_dir}/modules" ]] && du -sh "${git_dir}/modules" || echo "${git_dir}/modules is not present"
+
 
 declare -A head_blobs_map
 if [[ ${invest_remote_branches} == true ]]; then
   echo "Reading branch  blobs.."
-  if [[ $(git branch -r) != "" ]] ;then
+  if [[ $(git branch ${branch_remote_option}) != "" ]] ;then
     while read -r branch; do
       if [[ $branch == "" ]] ; then
         echo "branch variable is empty - skip"
@@ -145,12 +183,12 @@ if [[ ${invest_remote_branches} == true ]]; then
       if [[ ${second:-} == "" ]] ; then
         printf "LEAF: %s : ( #commits/files: %s/%s ) : %s\n" \
                                   "${branch}" \
-                                  "$( git log --oneline --format=%H $(git merge-base origin/HEAD ${branch} )..${branch} | wc -l )" \
-                                  "$( git diff-tree -r $(git merge-base origin/HEAD ${branch} )..${branch} | cut -f 4- -d ' ' | wc -l )" \
+                                  "$( git log --oneline --format=%H $(git merge-base ${default_branch} ${branch} )..${branch} | wc -l )" \
+                                  "$( git diff-tree -r $(git merge-base ${default_branch} ${branch} )..${branch} | cut -f 4- -d ' ' | wc -l )" \
                                   "$( git log --oneline --decorate -1 ${branch} )" \
-                                | tee -a ${WORKSPACE}/branches_leaves.txt
+                                | tee -a ${file_output_branch_leaves}
       else
-        printf "EMBEDDED: %s - skip : %s\n\n" "${branch}" "$( git log --oneline --decorate -1 ${branch} )" | tee -a "${WORKSPACE}/branches_embedded.txt"
+        printf "EMBEDDED: %s - skip : %s\n\n" "${branch}" "$( git log --oneline --decorate -1 ${branch} )" | tee -a "${file_output_branch_embedded}"
         continue
       fi
       # shellcheck disable=SC2046
@@ -161,12 +199,12 @@ if [[ ${invest_remote_branches} == true ]]; then
         head_file=${head_blob_line_array[2]}
         head_blobs_map["${head_blob}"]="${head_file}"
         [[ ${progress:-} == "true" ]] && printf "."
-      done < <( git diff-tree -r $(git merge-base origin/HEAD ${branch} )..${branch} | cut -f 4- -d ' ')
+      done < <( git diff-tree -r $(git merge-base ${default_branch} ${branch} )..${branch} | cut -f 4- -d ' ')
       printf "\n"
-    done < <( git branch -r | cut -f 3 -d ' '  | grep -v ".*/HEAD$")
+    done < <( git branch ${branch_remote_option} | cut -f 3 -d ' '  | grep -v "origin/HEAD$")
     printf "\nMake tagged branches lists: "
-    grep " (tag: " "${WORKSPACE}/branches_leaves.txt" > "${WORKSPACE}/branches_leaves_tagged.txt" &>2 / dev/null || echo "INFO: No leaf branches with tags"
-    grep " (tag: " "${WORKSPACE}/branches_embedded.txt" > "${WORKSPACE}/branches_embedded_tagged.txt" &>2 / dev/null || echo "INFO: No embbedded branches with tags"
+    grep " (tag: " "${file_output_branch_leaves}" > "${file_output_branch_leaves_tagged}" 2> /dev/null || echo "INFO: No leaf branches with tags"
+    grep " (tag: " "${file_output_branch_embedded}" > "${file_output_branch_embedded_tagged}" 2> /dev/null || echo "INFO: No embedded branches with tags"
     printf "Done\n\n"
   else
     printf "No remote branches found - skip\n"
@@ -177,18 +215,17 @@ fi
 
 echo
 
-git rev-list --objects --all  > "${WORKSPACE}/allfileshas.txt"
-cat "${WORKSPACE}/allfileshas.txt" | cut -d ' ' -f 2- | uniq > "${WORKSPACE}/allfileshas_uniq.txt"
+git rev-list --objects --all  > "${file_tmp_allfileshas}"
 
 printf "\n"
-touch "${WORKSPACE}/bigtosmall_sorted_size_files.txt"
+touch "${file_output_sorted_size_files}"
 echo "Investigate blobs that are directly stored in idx file: ${pack_file}"
-if [[ ! $(grep -E "^[a-f0-9]{40}[[:space:]]blob[[:space:]]+[0-9]+[[:space:]][0-9]+[[:space:]][0-9]+$" "${WORKSPACE}/verify_pack.txt" | awk -F" " '{print $1,$2,$3,$4,$5}' > "${WORKSPACE}/bigobjects.txt") ]]; then
-  printf "Amount of objects: %s\n" "$(wc -l < "${WORKSPACE}/bigobjects.txt")"
-  join <(sort "${WORKSPACE}/bigobjects.txt") <(sort "${WORKSPACE}/allfileshas.txt") | sort -k 3 -n -r | cut -f 1,3,6-  -d ' ' > "${WORKSPACE}/bigtosmall_join.txt"
+if [[ ! $(grep -E "^[a-f0-9]{40}[[:space:]]blob[[:space:]]+[0-9]+[[:space:]][0-9]+[[:space:]][0-9]+$" "${file_verify_pack}" | awk -F" " '{print $1,$2,$3,$4,$5}' > "${file_tmp_bigobjects}") ]]; then
+  printf "Amount of objects: %s\n" "$(wc -l < "${file_tmp_bigobjects}")"
+  join <(sort "${file_tmp_bigobjects}") <(sort "${file_tmp_allfileshas}") | sort -k 3 -n -r | cut -f 1,3,6-  -d ' ' > "${file_tmp_bigtosmall_join}"
 
   touch "${WORKSPACE}/bigtosmall_join_uniq.txt"
-  cat "${WORKSPACE}/bigtosmall_join.txt" |  cut -d ' ' -f 3- > "${WORKSPACE}/bigtosmall_join_all.txt"
+  cat "${file_tmp_bigtosmall_join}" |  cut -d ' ' -f 3- > "${WORKSPACE}/bigtosmall_join_all.txt"
   cat -n "${WORKSPACE}/bigtosmall_join_all.txt" | /usr/bin/sort -uk2 | /usr/bin/sort -n | cut -f2- > "${WORKSPACE}/bigtosmall_join_uniq.txt"
   amount_total_unique=$(wc -l < "${WORKSPACE}/bigtosmall_join_uniq.txt")
   printf "Amount of unique <path>/<file>: %s\n" "${amount_total_unique}"
@@ -208,23 +245,23 @@ while read -r file; do
     if [[ "${head_blobs_map[${blob}]:-}" == "$path_file" ]]; then
       prefix="R"
     fi
-    echo "$prefix $blob $size $path_file" >> "${WORKSPACE}/bigtosmall_sorted_size_files.txt"
+    echo "$prefix $blob $size $path_file" >> "${file_output_sorted_size_files}"
   done < <( file="${file//'.'/\\.}" && \
             file=${file//'*'/\\*} && \
             file=${file//'+'/\\+} && \
             file=${file//'?'/\\?} && \
             file=${file//'('/\\(} && \
             file=${file//')'/\\)} && \
-            grep -E " ${file}$" "${WORKSPACE}/bigtosmall_join.txt" || echo "ERROR: $file: something went wrong" >> "${WORKSPACE}/bigtosmall_errors.txt")
+            grep -E " ${file}$" "${file_tmp_bigtosmall_join}" || echo "ERROR: $file: something went wrong" >> "${WORKSPACE}/bigtosmall_errors.txt")
 
 done < "${WORKSPACE}/bigtosmall_join_uniq.txt"
 printf "\n\n"
 
-touch "${WORKSPACE}/bigtosmall_sorted_size_files_revisions.txt"
+touch "${file_output_sorted_size_files_revisions}"
 echo "Investigate blobs that are packed in revisions in idx file: ${pack_file}"
-if [[ ! $(grep -E "^[a-f0-9]{40}[[:space:]]blob[[:space:]]+[0-9]+[[:space:]][0-9]+[[:space:]][0-9]+[[:space:]][0-9]+[[:space:]][a-f0-9]{40}$" "${WORKSPACE}/verify_pack.txt" | awk -F" " '{print $1,$2,$3,$4,$5}' > "${WORKSPACE}/bigobjects_revisions.txt") ]]; then
-  printf "Amount of objects: %s\n" $(wc -l < "${WORKSPACE}/bigobjects_revisions.txt")
-  join <(sort "${WORKSPACE}/bigobjects_revisions.txt") <(sort "${WORKSPACE}/allfileshas.txt") | sort -k 3 -n -r | cut -f 1,3,6- -d ' '  > "${WORKSPACE}/bigtosmall_revisions_join.txt"
+if [[ ! $(grep -E "^[a-f0-9]{40}[[:space:]]blob[[:space:]]+[0-9]+[[:space:]][0-9]+[[:space:]][0-9]+[[:space:]][0-9]+[[:space:]][a-f0-9]{40}$" "${file_verify_pack}" | awk -F" " '{print $1,$2,$3,$4,$5}' > "${file_tmp_bigobjects_revisions}") ]]; then
+  printf "Amount of objects: %s\n" $(wc -l < "${file_tmp_bigtosmall_revisions}")
+  join <(sort "${file_tmp_bigtosmall_revisions}") <(sort "${file_tmp_allfileshas}") | sort -k 3 -n -r | cut -f 1,3,6- -d ' '  > "${WORKSPACE}/bigtosmall_revisions_join.txt"
   touch "${WORKSPACE}/bigtosmall_revisions_join_uniq.txt"
   cat "${WORKSPACE}/bigtosmall_revisions_join.txt" |  cut -d ' ' -f 3- > "${WORKSPACE}/bigtosmall_revisions_join_all.txt"
   cat -n "${WORKSPACE}/bigtosmall_revisions_join_all.txt" | /usr/bin/sort -uk2 | /usr/bin/sort -n | cut -f2- > "${WORKSPACE}/bigtosmall_revisions_join_uniq.txt"
@@ -245,7 +282,7 @@ while read -r file; do
     if [[ "${head_blobs_map[${blob}]:-}" == "$file" ]]; then
       prefix="R"
     fi
-    echo "$prefix $blob $size $path_file" >> "${WORKSPACE}/bigtosmall_sorted_size_files_revisions.txt"
+    echo "$prefix $blob $size $path_file" >> "${file_output_sorted_size_files_revisions}"
   done < <(file="${file//'.'/\\.}" && file=${file//'*'/\\*} && file=${file//'+'/\\+} && file=${file//'?'/\\?}  && file=${file//'('/\\(} && file=${file//')'/\\)} && grep -E " ${file}$" "${WORKSPACE}/bigtosmall_revisions_join.txt"  || echo "ERROR: $file: something went wrong" >> "${WORKSPACE}/bigtosmall_errors_revision.txt")
 done < "${WORKSPACE}/bigtosmall_revisions_join_uniq.txt"
 printf "\n\n"
